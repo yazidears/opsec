ca7638c51303185e8a80c6331240053e7934617b
yzde <74788670+yazidears@users.noreply.github.com>
2026-08-20 14:37:18 +0200

    release opsec formula and repository wording

--- Formula/opsec.rb
+++ Formula/opsec.rb
@@ -1,8 +1,8 @@
 class Opsec < Formula
   desc "Animated operation-security meme for the terminal"
   homepage "https://github.com/yazidears/opsec"
-  url "https://github.com/yazidears/opsec/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "d49548d8f1e3f032a9da0efd4ed6a94e33987461e8b95d641fa2c0ec4fffe607"
+  url "https://github.com/yazidears/opsec/releases/download/v0.1.1/opsec-0.1.1.tar.gz"
+  sha256 "e4fba96a1f5ee9ac03ee61e3ac70573725e46427bd6f99337a89ddf611a3a203"
   license "MIT"
 
   def install
@@ -11,7 +11,7 @@ class Opsec < Formula
   end
 
   test do
-    assert_match "opsec 0.1.0", shell_output("#{bin}/opsec --version")
-    assert_match "OPSEC EXPRESS", shell_output("#{bin}/opsec --once")
+    assert_match "opsec 0.1.1", shell_output("#{bin}/opsec --version")
+    assert_match "OPERATION SECURITY", shell_output("#{bin}/opsec --once")
   end
 end
